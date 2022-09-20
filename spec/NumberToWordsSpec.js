ad66ce87f0f561a1fe4763ca2fd0c0c58356f1af
vikrant17 <vikrant@spliceglobal.com>
2022-09-20 23:26:59 +0530

    number to word converter for 3 digit completed

--- spec/NumberToWordsSpec.js
+++ spec/NumberToWordsSpec.js
@@ -8,9 +8,34 @@ describe("convert a give number of words", function() {
     })
     it("converts numbers to words", function() {
         expect(null).toEqual(numberToWordConverter.convert(null));
-        expect(null).toEqual(numberToWordConverter.convert(''));
-        expect("zero").toEqual(numberToWordConverter.convert(0));
+        expect("").toEqual(numberToWordConverter.convert(''));
+        expect("negative one").toEqual(numberToWordConverter.convert(-1));
         expect("one").toEqual(numberToWordConverter.convert(1));
+        expect("five").toEqual(numberToWordConverter.convert(5));
+        expect("ten").toEqual(numberToWordConverter.convert(10));
+        expect("eleven").toEqual(numberToWordConverter.convert(11));
+        expect("nineteen").toEqual(numberToWordConverter.convert(19));
+        expect("twenty").toEqual(numberToWordConverter.convert(20));
+        expect("twenty one").toEqual(numberToWordConverter.convert(21));
+        expect("ninety nine").toEqual(numberToWordConverter.convert(99));
+        expect("one hundred one").toEqual(numberToWordConverter.convert(101));
+        expect("one hundred fifty").toEqual(numberToWordConverter.convert(150));
+        expect("five hundred").toEqual(numberToWordConverter.convert(500));
+        expect("nine hundred ninety nine").toEqual(numberToWordConverter.convert(999));
+        expect("one thousand").toEqual(numberToWordConverter.convert(1000));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
