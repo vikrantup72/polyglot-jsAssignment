488f53e743422586daed4020c39ac624e28d9591
vikrant17 <vikrant@spliceglobal.com>
2022-09-22 11:37:48 +0530

    Check day sun to mon completed with test cases

--- spec/Check_DaySpec.js
+++ spec/Check_DaySpec.js
@@ -6,7 +6,9 @@ describe("Check Day", function(){
     });
 
     it("Checks Day", function() {
-    	expect("null").toBe(checkDay.checks(0));
+    	expect(null).toBe(checkDay.checks(null));
+    	expect('').toBe(checkDay.checks(''));
+    	expect(0).toBe(checkDay.checks(0));
     	expect("Sunday").toBe(checkDay.checks(1));
     	expect("Monday").toBe(checkDay.checks(2));
     	expect("Tuesday").toBe(checkDay.checks(3));
