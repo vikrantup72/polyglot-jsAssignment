98d3b5113ad8c1d5b660ca8d248face909e30da0
vikrant17 <vikrant@spliceglobal.com>
2022-09-27 21:19:45 +0530

    largest two number and three number

--- spec/LargestSpec.js
+++ spec/LargestSpec.js
@@ -4,21 +4,16 @@ describe("Testing Two Numbers", function() {
     beforeEach(function() {
       find = new Find();
     });
-    it("Add Numbers", function() {
-        expect(find.AddTwoNumbers(4,3)).toEqual(7);
-    });
-    it("Multiply Numbers", function() {
-        expect(find.MultiplyTwoNumbers(4,3)).toEqual(12);
-    });
- 
-    it("Compare Numbers to be Greater Than", function() {
-    	expect(4).toBeGreaterThan(3);
-    });
- 
-    it("Compare Numbers to be Less Than", function() {
-    	expect(3).toBeLessThan(4);
+    it("Compare two Numbers", function() {
+        expect(null).toBe(find.TwoNumber(null));
+        expect(null).toBe(find.TwoNumber(''));
+        expect(4).toBe(find.TwoNumber(4,3));
+        expect(7).toBe(find.TwoNumber(5,7));
+
     });
     it("Compare three Numbers", function() {
+        expect(null).toBe(find.TwoNumber(null));
+        expect(null).toBe(find.TwoNumber(''));
         expect(4).toBe(find.ThreeNumber(4,3,2));
         expect(7).toBe(find.ThreeNumber(5,7,7));
 
