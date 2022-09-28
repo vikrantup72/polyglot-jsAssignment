e2654acb936e13028358342079ff7c6c60a76112
vikrant17 <vikrant@spliceglobal.com>
2022-09-28 09:58:27 +0530

    factorial completed with test cases

---
+++ spec/FactorialSpec.js
@@ -0,0 +1,14 @@
+describe("Fcatorial Number", function() {
+    var Fact = require('../lib/FactorialNumber');
+    var fact;
+    beforeEach(function() {
+        fact = new Fact();
+    });
+    it("fact Numbers", function() {
+        expect(null).toBe(fact.find(null));
+        expect('').toBe(fact.find(''));
+        expect(6).toBe(fact.find(3));
+        expect(120).toBe(fact.find(5));
+        expect(3628800).toBe(fact.find(10));
+    });
+});
