079a6994904c949b25da6d1f7228ab00fba95b7e
vikrant17 <vikrant@spliceglobal.com>
2022-09-29 07:45:45 +0530

    factore completed with test cases

--- spec/FactorSpec.js
+++ spec/FactorSpec.js
@@ -4,11 +4,12 @@ describe("TestingFactor Number", function() {
     beforeEach(function() {
         factorNum = new FactorNum();
     });
-    it("Compare three Numbers", function() {
-        expect(null).toBe(factorNum.check(null));
-        expect('').toBe(factorNum.check(''));
-        expect('1,3').toBe(factorNum.check(3));
-        expect('1,2,3,4,6,12').toBe(factorNum.check(12));
+    it("Fcator App Numbers", function() {
+        expect(factorNum.check(null)).toBe(null);
+        expect(factorNum.check('')).toBe('');
+        expect(factorNum.check(3)).toBe('1,3');
+        expect(factorNum.check(4)).toBe('1,2,4');
+        expect(factorNum.check(12)).toBe('1,2,3,4,6,12');
 
     });
 });
