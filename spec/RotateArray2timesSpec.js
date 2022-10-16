34c4e744db538458e9da8caed3c8aede58fd1d6d
vikran17 <vikrant@spliceglobal.com>
2022-10-16 21:28:57 +0530

    LeftRotate Array 2 times completed with test cases

---
+++ spec/RotateArray2timesSpec.js
@@ -0,0 +1,15 @@
+describe("Left Rotate Array ", () => {
+  var LeftRotate = require("../lib/RotateArray2times");
+  var leftRotate;
+
+  beforeEach(function () {
+    leftRotate = new LeftRotate();
+  });
+  it("Left Rotate Array  Elements", function () {
+    expect(leftRotate.rotateLeft(null)).toEqual(null);
+    expect(leftRotate.rotateLeft("")).toEqual("");
+    expect(leftRotate.rotateLeft([1, 2], 2)).toEqual([1, 2]);
+    expect(leftRotate.rotateLeft([1, 2, 3], 2)).toEqual([3, 1, 2]);
+    expect(leftRotate.rotateLeft([1, 2, 3, 4, 5], 2)).toEqual([3, 4, 5, 1, 2]);
+  });
+});
