2c20b1c9c938f7b00ebf172e9433b8e6e0737535
vikran17 <vikrant@spliceglobal.com>
2022-11-01 07:45:50 +0530

    stack completed

--- spec/StackSpec.js
+++ spec/StackSpec.js
@@ -1,16 +1,25 @@
-describe("Satck App array", () => {
-    var Stack = require("../lib/Stack");
-    var stack;
-  
-    beforeEach(function () {
-      stack = new Stack();
-    });
-    it("Stack App", function () {
-      expect(stack.ready(null)).toEqual(null);
-      expect(stack.ready("")).toEqual("");
-      expect(stack.ready([])).toEqual([]);
-      expect(stack.ready([1,4,6],2)).toEqual([1]);
-      expect(stack.ready([1,4,6,5,2,1],4)).toEqual([1,4,6]);
-    });
+describe("Stack App array", () => {
+  var Stack = require("../lib/Stack");
+  var stack;
+
+  beforeEach(function () {
+    stack = new Stack();
   });
-  
+
+  const addArray = (arr, poptime) => {
+    let newArr;
+    stack.clear();
+    for (let i = 0; i < arr.length; i++) {
+      stack.add(arr[i]);
+    }
+    for (let i = 1; i <= poptime; i++) {
+      newArr = stack.remove();
+    }
+    return newArr;
+  };
+  it("stack App", function () {
+    expect(addArray([12], 1)).toEqual([]);
+    expect(addArray([12, 24], 1)).toEqual([12]);
+    expect(addArray([12, 24, 11, 13], 2)).toEqual([12,24]);
+  });
+});
