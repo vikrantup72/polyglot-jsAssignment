f3081a3b3cc8ccf55d80acdbdd975ee43a9fea28
vikran17 <vikrant@spliceglobal.com>
2022-11-01 07:44:49 +0530

    updated

--- spec/QueueSpec.js
+++ spec/QueueSpec.js
@@ -1,16 +1,25 @@
 describe("Queue App array", () => {
-    var Queue = require("../lib/Queue");
-    var queue;
-  
-    beforeEach(function () {
-        queue = new Queue();
-    });
-    it("Queue App", function () {
-      expect(queue.ready(null)).toEqual(null);
-      expect(queue.ready("")).toEqual("");
-      expect(queue.ready([])).toEqual([]);
-      expect(queue.ready([1,4,6],2)).toEqual([6]);
-      expect(queue.ready([1,4,6,5,2,1],4)).toEqual([5,2,1]);
-    });
+  var Queue = require("../lib/Queue");
+  var queue;
+
+  beforeEach(function () {
+    queue = new Queue();
   });
-  
+
+  const addArray = (arr, poptime) => {
+    let newArr;
+    queue.clear();
+    for (let i = 0; i < arr.length; i++) {
+      queue.add(arr[i]);
+    }
+    for (let i = 1; i <= poptime; i++) {
+      newArr = queue.remove();
+    }
+    return newArr;
+  };
+  it("queue App", function () {
+    expect(addArray([12], 1)).toEqual([]);
+    expect(addArray([12, 24], 1)).toEqual([24]);
+    expect(addArray([12, 24, 11, 13], 2)).toEqual([11, 13]);
+  });
+});
